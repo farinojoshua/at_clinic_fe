be3b418cf58bde2d620076a3413f25c3288f62d6
Diffa <you@example.com~>
2025-05-21 13:33:23 +0700

    login register page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <h1>Ini Navbar</h1>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
+      <div className="bg-red-500">
+        <h1>Ini Navbar</h1>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+
+      </div>
     </>
   );
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Navbar from "../components/Navbar";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
