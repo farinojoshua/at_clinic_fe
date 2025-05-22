18e65306ba67a3dcf6b83dba34a59dd08f685dc3
Farino Joshua <96770446+farinojoshua@users.noreply.github.com>
2025-05-22 14:24:44 +0700

    change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -93,12 +93,12 @@ export default function Login() {
 
           <p className='mt-10 text-center text-sm text-gray-500'>
             Belum Mendaftar?{" "}
-            <a
-              href='/register'
+            <Link
+              to='/register'
               className='font-semibold text-blue-600 hover:text-blue-500'
             >
               Daftar
-            </a>
+            </Link>
           </p>
         </div>
       </div>
